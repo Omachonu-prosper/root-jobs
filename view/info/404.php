bf89e371999c8bc68cb290eae9794af8d7eaa1f3
Omachonu Prosper <omachonucodes@gmail.com>
2022-09-26 09:51:26 +0100

    Create 404 page for protected urls

---
+++ view/info/404.php
@@ -0,0 +1,14 @@
+<?php include_once '../templates/header.php' ?>
+
+	<div class="mx-auto p-3" style="max-width:50rem">
+		<h1 class="display-1">
+			404 Not Found
+		</h1>
+		
+		<p>
+			The page you requested was not foud on this server.
+			<a href="/">Go Home</a>
+		</p>
+	</div>
+
+<?php include_once '../templates/footer.php' ?>
