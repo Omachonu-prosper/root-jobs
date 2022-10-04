602790a928468bad377394939a9cbd069e1bbfef
Omachonu Prosper <omachonucodes@gmail.com>
2022-10-04 20:12:30 +0100

    Require a password to delete user account

--- view/user/account.php
+++ view/user/account.php
@@ -2,10 +2,7 @@
 	session_start();
 
 	// If no user is logged in Redirect them to the login page
-	if(isset($_SESSION['current_user'])) {
-		// echo $_SESSION['current_user']['id'];
-	}
-	else {
+	if(!isset($_SESSION['current_user'])) {
 		header('location: /view/auth/login.php');
 	}
 
@@ -125,7 +122,21 @@
 			</form>
 		</div>
 
-		<div class="my-5 text-danger card border-danger">
+		<div class="my-5 text-danger card border-danger" id="delete-section">
+			<!-- Let the user know that the password they inputed is wrong -->
+			<?php if(isset($_SESSION['message_to_show'])) { ?>
+				<div class="alert alert-danger text-center mb-3">
+					<?php 
+						echo $_SESSION['message_to_show']; 
+						unset($_SESSION['message_to_show']);
+					?>
+
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+			<?php } ?>
+
 			<div class="card-body">
 				<h2>Danger Zone <i class="fa fa-exclamation-triangle"></i></h2>
 				<div class="card-sub-title my-3">
@@ -135,9 +146,19 @@
 				<div class="popup-canvas hidden" id="user-delete-confirmation-canvas">
 					<div class="shadowed-card card popup-background-blur" id="confirm-user-delete">
 						<div class="card-body">
-							<p>You are about to delete your account "<?php echo $_SESSION['current_user']['username'] ?>". You can not undo this action and all your data will be removed. Are you sure you want to continue?</p>
+							<p>
+								You are about to delete your account "<?php echo $_SESSION['current_user']['username'] ?>". You can not undo this action and all your data will be removed. Enter your password to continue?
+							</p>
 
-							<form action="/controller/user/delete.php" method="post">
+							<form action="/controller/user/delete.php" method="post" novalidate class="needs-validation">
+								<div class="form-group">
+									<input type="password" class="form-control" placeholder="Please enter your password" id="username" required name="account_delete_password">
+
+									<div class="invalid-feedback">
+									    Please enter a password
+								    </div>
+								</div>
+
 								<a href="#!" id="cancel-user-delete" class="btn btn-outline-success mr-3">
 									Cancel
 								</a>
